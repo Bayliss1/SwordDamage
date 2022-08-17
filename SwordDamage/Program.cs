c36501fe684a63c882eeb516e3bdec188012b27e
Owen <owen.bayliss@sky.com>
2022-08-17 22:12:58 +0100

    Add project files.

---
+++ SwordDamage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwordDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
